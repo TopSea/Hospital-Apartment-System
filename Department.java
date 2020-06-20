52473bb2df9609561b02ece566899a507f5f131e
TopSea <44743008+TopSea@users.noreply.github.com>
2020-06-20 17:17:53 +0800

    Update Department.java

--- Department.java
+++ Department.java
@@ -4,33 +4,27 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import static java.lang.System.out;
 
 public class Department extends JFrame implements ActionListener {
     private JTable jt = null;
     private JScrollPane jsp = null;
-    private JButton select, insert, delete, updata;
+    private JButton select, insert, delete, updata1, updata2, updata3;
     private JPanel north, south, all;
     private JTextField nameText;
     private JLabel tip;
-    String name, sql, room;
-    int row;
+    private JTextArea infos;
+    String name, sql, num;
     DepartModel myModel = new DepartModel();
-    
+
     public  JPanel Department() {
 
         north = new JPanel();
         south = new JPanel();
         all = new JPanel();
 
-        tip = new JLabel("请输入科室编号进行查询");
-        nameText = new JTextField(10);
-        select = new JButton("查询");
-        select.setActionCommand("查询");
-        select.addActionListener(this);
-        north.add(tip);
-        north.add(nameText);
-        north.add(select);
+        infos = new JTextArea(25, 45);
 
         insert = new JButton("添加");
         insert.setActionCommand("添加");
@@ -38,22 +32,31 @@ public class Department extends JFrame implements ActionListener {
         delete = new JButton("删除");
         delete.setActionCommand("删除");
         delete.addActionListener(this);
-        updata = new JButton("更新");
-        updata.setActionCommand("更新");
-        updata.addActionListener(this);
+
+        updata1 = new JButton("科室一");
+        updata1.setActionCommand("科室一");
+        updata1.addActionListener(this);
+        updata2 = new JButton("科室二");
+        updata2.setActionCommand("科室二");
+        updata2.addActionListener(this);
+        updata3 = new JButton("科室三");
+        updata3.setActionCommand("科室三");
+        updata3.addActionListener(this);
+
+        north.add(updata1);
+        north.add(updata2);
+        north.add(updata3);
+
         south.add(insert);
-        south.add(updata);
         south.add(delete);
 
         myModel = new DepartModel();
-        jt = new JTable(myModel);
-        jsp = new JScrollPane(jt);
+        jsp = new JScrollPane(infos);
         all.add(north, BorderLayout.NORTH);
         all.add(jsp);
         all.add(south, BorderLayout.SOUTH);
         return all;
     }
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -70,18 +73,23 @@ public class Department extends JFrame implements ActionListener {
             case "删除":
                 JOptionPane.showMessageDialog(this, "无此权限！");
                 break;
-            case "更新":
-                row = this.jt.getSelectedRow();
-                out.println(row);
-                if (row == -1) {
-                    JOptionPane.showMessageDialog(this, "请选择一行！");
-                    return;
-                }
-                room = (String) myModel.getValueAt(row, 0);
+            case "科室一":
+                num = "D01";
                 myModel = new DepartModel();
-                myModel.UpdateDepart("update department set room_info = ? where number = ?", room);
-                DepartModel my = new DepartModel();
-                jt.setModel(my);
+                infos.setText(myModel.UpdateDepart("update department set room_info = ? where number = ?", num));
+                infos.setFont(new Font("宋体", Font.PLAIN, 17));
+                break;
+            case "科室二":
+                num = "D02";
+                myModel = new DepartModel();
+                infos.setText(myModel.UpdateDepart("update department set room_info = ? where number = ?", num));
+                infos.setFont(new Font("宋体", Font.PLAIN, 17));
+                break;
+            case "科室三":
+                num = "D03";
+                myModel = new DepartModel();
+                infos.setText(myModel.UpdateDepart("update department set room_info = ? where number = ?", num));
+                infos.setFont(new Font("宋体", Font.PLAIN, 17));
                 break;
             default:
                 break;
